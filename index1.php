2b2863b6ee9f2a53a622d5cedb9e854e8063ff01
yuniarti11 <yuniarti.sisfo@student.untan.ac.id>
2018-03-14 17:34:19 +0700

    latihan satu halaman perdana

---
+++ index1.php
@@ -0,0 +1,16 @@
+<?php 
+// ini adalah komentar saja 
+// dilarang komen 
+// kalau like boleh
+
+	echo "yuniarti<br>";
+	echo "NIM H1101161019<br>";
+
+	echo "apa kabar dunia?<br>";
+
+	$tinggi = 160;
+
+	echo "tinggi badan saya adalah","\n $tinggi";
+	
+
+ ?>
